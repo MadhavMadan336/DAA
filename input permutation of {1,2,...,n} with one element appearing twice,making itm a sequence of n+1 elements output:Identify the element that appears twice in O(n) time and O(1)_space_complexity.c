038caf8f68366f1d0ecb8642b574d879329383fc
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-09-01 21:27:24 +0530

    Create input permutation of {1,2,...,n} with one element appearing twice,making itm a sequence of n+1 elements output:Identify the element that appears twice in O(n) time and O(1)_space_complexity.c

---
+++ input permutation of {1,2,...,n} with one element appearing twice,making itm a sequence of n+1 elements output:Identify the element that appears twice in O(n) time and O(1)_space_complexity.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int SearchDuplicate(int arr[], int size) {
+    int n = size - 1;
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    int Natural_No_sum = (n * (n + 1)) / 2;
+    int duplicated_element = sum - Natural_No_sum;
+    return duplicated_element;
+}
+
+int main() {
+    int size;
+    printf("Enter Size: ");
+    scanf("%d", &size);
+
+    int *array = (int *)malloc(size * sizeof(int));
+    if (!array) {
+        perror("Memory allocation failed");
+        return 1;
+    }
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    int duplicated_element = SearchDuplicate(array, size);
+    printf("The duplicated element is: %d\n", duplicated_element);
+
+    // Free allocated memory
+    free(array);
+
+    return 0;
+}
