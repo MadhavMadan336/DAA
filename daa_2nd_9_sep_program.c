f3e5e0e827325888c4bc0e489e6ece837e5e05cf
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-09-09 00:05:54 +0530

    Create daa_2nd_9_sep_program.c

---
+++ daa_2nd_9_sep_program.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <complex.h>
+
+double complex complex_power(int n) {
+    // Base case: (1+i)^0 = 1
+    if (n == 0) {
+        return 1 + 0 * I;
+    }
+
+    // Recursive case
+    double complex result = complex_power(n / 2);
+    result = result * result;
+
+    // If n is odd, multiply by (1+i)
+    if (n % 2 == 1) {
+        result = result * (1 + I);
+    }
+
+    return result;
+}
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    double complex result = complex_power(n);
+    printf("The value of (1+i)^%d is: %.2f + %.2fi\n", n, creal(result), cimag(result));
+
+    return 0;
+}
